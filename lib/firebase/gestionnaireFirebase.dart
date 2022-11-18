40fce005a72b5b3d0bf89c0f5b39f3148e4eb19b
Serifou Ahmed Abdoul-Kader <serifouabdoulkader@icloud.com>
2022-11-18 16:53:14 +0000

    authentification

---
+++ lib/firebase/gestionnaireFirebase.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class GestionnaireFirbase{
+  FirebaseAuth authInstance=FirebaseAuth.instance;
+  //connexion
+  Future<User>Connextion({required String email,required String mdp})async{
+    User? users=(await authInstance.signInWithEmailAndPassword(email: email, password:mdp)).user;
+    return users!;
+  }
+  //inscription
+  Future<User>Enregistrer({required String email,required String mdp,required String nom,required String prenom})async{
+    User?users=(await(authInstance.createUserWithEmailAndPassword(email: email, password:mdp))).user;
+    return users!;
+  }
+}
